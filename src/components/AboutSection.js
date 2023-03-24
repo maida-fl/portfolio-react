c93e331c50a6af2adbfee8dd2af7f965f6701e13
maida-fl <maida.guzman@outlook.com>
2023-03-24 17:42:06 -0300

    framer motion - page transitions

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -4,41 +4,20 @@ import { About, Description, Image, Hide } from "../styles";
 import { motion } from "framer-motion";
 
 const AboutSection = () => {
-  const titleAnim = {
-    hidden: { opacity: 0 },
-    show: { opacity: 1, transition: { duration: 1 } },
-  };
-  const container = {
-    hidden: { x: 100 },
-    show: {
-      x: 0,
-      transition: {
-        duration: 0.75,
-        ease: "easeOut",
-        staggerChildren: 1,
-        when: "beforeChildren",
-      },
-    },
-  };
   return (
     <About>
       <Description>
-        <motion.div
-          variants={container}
-          initial="hidden"
-          animate="show"
-          className="title"
-        >
+        <motion.div>
           <Hide>
-            <motion.h2 variants={titleAnim}>We work to make</motion.h2>
+            <motion.h2>We work to make</motion.h2>
           </Hide>
           <Hide>
-            <motion.h2 variants={titleAnim}>
+            <motion.h2>
               your <span>dreams</span> come
             </motion.h2>
           </Hide>
           <Hide>
-            <motion.h2 variants={titleAnim}>true.</motion.h2>
+            <motion.h2>true.</motion.h2>
           </Hide>
         </motion.div>
         <p>
